73b4874c21f90fcbb383a073b6c4c33c1351c8f2
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2020-12-19 15:34:51 +0900

    Add files via upload

--- Tolo/src/tolo/Main.java
+++ Tolo/src/tolo/Main.java
@@ -1,20 +1,19 @@
 package tolo;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
-    int num = 0;
-
     Main() {
-        Frame frame = new Frame();
-        frame.setTitle("Tolo");
-        frame.setSize(640, 480); //4:3
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setAlwaysOnTop(true);
+        JFrame frame = new JFrame();
+        frame.setSize(640, 480);
         frame.setLocation(150, 150);
+        frame.setTitle("tolo");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setAlwaysOnTop(true);
+        frame.setLayout(null);
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -22,23 +21,69 @@ public class Main {
             }
         });
 
-        Label lbl = new Label();
-        lbl.setText("" + 0);
-        lbl.setBounds(frame.getWidth() / 2 - 35, frame.getHeight() / 2 - 70, 70, 50);
-        frame.add(lbl);
+        Label label = new Label();
+        label.setBounds(frame.getWidth() / 2 - 50, frame.getHeight() / 2 - 50, 150, 20);
+        label.setText("Hello :D");
+        label.setVisible(false);
+        frame.add(label);
 
-        Button btn = new Button();
-        btn.setBounds(frame.getWidth() / 2 - 50, frame.getHeight() / 2 - 20, 100, 40);
-        btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        btn.setLabel("Click me!");
-        btn.addMouseListener(new MouseAdapter() {
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(frame.getWidth() / 2 - 50, frame.getHeight() / 2, 100, 25);
+        frame.add(passwordField);
+
+        TextField field = new TextField();
+        field.setBounds(frame.getWidth() / 2 - 50, frame.getHeight() / 2 - 25, 100, 25);
+        frame.add(field);
+
+        Button button = new Button();
+        button.setBounds(frame.getWidth() / 2 + 55, frame.getHeight() / 2 - 20, 70, 35);
+        button.setLabel("log in");
+        button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                num += 1;
-                lbl.setText("" + num);
+                if (field.getText().equalsIgnoreCase("semin123")) {
+                    if (passwordField.getText().equalsIgnoreCase("psm135790.1")) {
+                        System.out.println("id: " + field.getText());
+                        System.out.println("password: " + passwordField.getText());
+                        System.out.println("-----welcome-----");
+                        field.setText("");
+                        passwordField.setText("");
+                        label.setVisible(false);
+
+                        JFrame frame2 = new JFrame();
+                        frame2.setSize(640, 480);
+                        frame2.setLocation(800, 150);
+                        frame2.setTitle("tolo (manage)");
+                        frame2.setResizable(false);
+                        frame2.setVisible(true);
+                        frame2.setAlwaysOnTop(true);
+                        frame2.setLayout(null);
+                        frame2.addWindowListener(new WindowAdapter() {
+                            @Override
+                            public void windowClosing(WindowEvent e) {
+                                System.exit(0);
+                            }
+                        });
+                    }
+                    else {
+                        label.setVisible(true);
+                        label.setBackground(Color.red);
+                        label.setText("id or password is wrong");
+                        field.setText("");
+                        passwordField.setText("");
+                    }
+                }
+                else {
+                    label.setVisible(true);
+                    label.setBackground(Color.red);
+                    label.setText("id or password is wrong");
+                    field.setText("");
+                    passwordField.setText("");
+                }
             }
         });
-        frame.add(btn);
+        frame.add(button);
     }
 
     public static void main(String[] args) {
